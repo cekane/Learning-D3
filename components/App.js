e28605652b2282bd9d109060fdd1962b0c659230
cekane <ckane144@gmail.com>
2016-07-06 11:16:49 -0400

    First Commit

---
+++ components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Container } from './Container'
+
+export class App extends React.Component {
+
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>You're good to go</h1>
+			</div>
+
+		);
+	}
+}
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+	entry: './main.js',
+	devtool: "source-map",
+	output: {
+		path: './',
+		filename: 'index.js'
+	},
+	devServer: {
+		inline: true,
+		port: 3333
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel',
+				query: {
+					presets: ['es2015', 'react']
+				}
+			},
+			{ 
+				test: /\.scss$/,
+				loaders: ["style", "css?modules", "sass"]
+    		},
+    		{
+		        test: /\.js$/,
+		        loader: "source-map-loader"
+		    }
+		]
+	}
+}
